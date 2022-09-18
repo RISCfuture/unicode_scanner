a4520e53ddeef6496b2515c2217acd7f1250a456
Tim Morgan <git@timothymorgan.info>
2022-09-18 12:34:24 -0700

    Spec syntax update

--- spec/unicode_scanner_spec.rb
+++ spec/unicode_scanner_spec.rb
@@ -5,202 +5,202 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe UnicodeScanner do
   it "should pass all the class-level examples" do
     s = UnicodeScanner.new('This is an example string')
-    s.eos?.should == false
+    expect(s.eos?).to eq(false)
 
-    s.scan(/\w+/).should == "This"
-    s.scan(/\w+/).should == nil
-    s.scan(/\s+/).should == " "
-    s.scan(/\s+/).should == nil
-    s.scan(/\w+/).should == "is"
-    s.eos?.should == false
+    expect(s.scan(/\w+/)).to eq("This")
+    expect(s.scan(/\w+/)).to eq(nil)
+    expect(s.scan(/\s+/)).to eq(" ")
+    expect(s.scan(/\s+/)).to eq(nil)
+    expect(s.scan(/\w+/)).to eq("is")
+    expect(s.eos?).to eq(false)
 
-    s.scan(/\s+/).should == " "
-    s.scan(/\w+/).should == "an"
-    s.scan(/\s+/).should == " "
-    s.scan(/\w+/).should == "example"
-    s.scan(/\s+/).should == " "
-    s.scan(/\w+/).should == "string"
-    s.eos?.should == true
+    expect(s.scan(/\s+/)).to eq(" ")
+    expect(s.scan(/\w+/)).to eq("an")
+    expect(s.scan(/\s+/)).to eq(" ")
+    expect(s.scan(/\w+/)).to eq("example")
+    expect(s.scan(/\s+/)).to eq(" ")
+    expect(s.scan(/\w+/)).to eq("string")
+    expect(s.eos?).to eq(true)
 
-    s.scan(/\s+/).should == nil
-    s.scan(/\w+/).should == nil
+    expect(s.scan(/\s+/)).to eq(nil)
+    expect(s.scan(/\w+/)).to eq(nil)
   end
 
   it "should pass the #concat example" do
     s = UnicodeScanner.new("Fri Dec 12 1975 14:39")
     s.scan(/Fri /)
     s << " +1000 GMT"
-    s.string.should == "Fri Dec 12 1975 14:39 +1000 GMT"
-    s.scan(/Dec/).should == "Dec"
+    expect(s.string).to eq("Fri Dec 12 1975 14:39 +1000 GMT")
+    expect(s.scan(/Dec/)).to eq("Dec")
   end
 
   it "should pass the #[] example" do
     s = UnicodeScanner.new("Fri Dec 12 1975 14:39")
-    s.scan(/(\w+) (\w+) (\d+) /).should == "Fri Dec 12 "
-    s[0].should == "Fri Dec 12 "
-    s[1].should == "Fri"
-    s[2].should == "Dec"
-    s[3].should == "12"
-    s.post_match.should == "1975 14:39"
-    s.pre_match.should == ""
+    expect(s.scan(/(\w+) (\w+) (\d+) /)).to eq("Fri Dec 12 ")
+    expect(s[0]).to eq("Fri Dec 12 ")
+    expect(s[1]).to eq("Fri")
+    expect(s[2]).to eq("Dec")
+    expect(s[3]).to eq("12")
+    expect(s.post_match).to eq("1975 14:39")
+    expect(s.pre_match).to eq("")
   end
 
   it "should pass the #beginning_of_line? example" do
     s = UnicodeScanner.new("test\ntest\n")
-    s.bol?.should == true
+    expect(s.bol?).to eq(true)
     s.scan(/te/)
-    s.bol?.should == false
+    expect(s.bol?).to eq(false)
     s.scan(/st\n/)
-    s.bol?.should == true
+    expect(s.bol?).to eq(true)
     s.terminate
-    s.bol?.should == true
+    expect(s.bol?).to eq(true)
   end
 
   it "should pass the #check example" do
     s = UnicodeScanner.new("Fri Dec 12 1975 14:39")
-    s.check(/Fri/).should == "Fri"
-    s.pos.should == 0
-    s.matched.should == "Fri"
-    s.check(/12/).should == nil
-    s.matched.should == nil
+    expect(s.check(/Fri/)).to eq("Fri")
+    expect(s.pos).to eq(0)
+    expect(s.matched).to eq("Fri")
+    expect(s.check(/12/)).to eq(nil)
+    expect(s.matched).to eq(nil)
   end
 
   it "should pass the #check_until example" do
     s = UnicodeScanner.new("Fri Dec 12 1975 14:39")
-    s.check_until(/12/).should == "Fri Dec 12"
-    s.pos.should == 0
-    s.matched.should == "12"
+    expect(s.check_until(/12/)).to eq("Fri Dec 12")
+    expect(s.pos).to eq(0)
+    expect(s.matched).to eq("12")
   end
 
   it "should pass the #eos? example" do
     s = UnicodeScanner.new('test string')
-    s.eos?.should == false
+    expect(s.eos?).to eq(false)
     s.scan(/test/)
-    s.eos?.should == false
+    expect(s.eos?).to eq(false)
     s.terminate
-    s.eos?.should == true
+    expect(s.eos?).to eq(true)
   end
 
   it "should pass the #exist? example" do
     s = UnicodeScanner.new('test string')
-    s.exist?(/s/).should == 3
-    s.scan(/test/).should == "test"
-    s.exist?(/s/).should == 2
-    s.exist?(/e/).should == nil
+    expect(s.exist?(/s/)).to eq(3)
+    expect(s.scan(/test/)).to eq("test")
+    expect(s.exist?(/s/)).to eq(2)
+    expect(s.exist?(/e/)).to eq(nil)
   end
 
   it "should pass a tweaked version of the #getch example" do
     s = UnicodeScanner.new("ab")
-    s.getch.should == "a"
-    s.getch.should == "b"
-    s.getch.should == nil
+    expect(s.getch).to eq("a")
+    expect(s.getch).to eq("b")
+    expect(s.getch).to eq(nil)
 
     s = UnicodeScanner.new("ぁ")
-    s.getch.should == "ぁ" # Japanese hira-kana "A" in EUC-JP
-    s.getch.should == nil
+    expect(s.getch).to eq("ぁ") # Japanese hira-kana "A" in EUC-JP
+    expect(s.getch).to eq(nil)
   end
 
   it "should pass the #inspect example" do
     s = UnicodeScanner.new("Fri Dec 12 1975 14:39")
-    s.inspect.should == '#<UnicodeScanner 0/21 @ "Fri D...">'
-    s.scan_until(/12/).should == "Fri Dec 12"
-    s.inspect.should == '#<UnicodeScanner 10/21 "...ec 12" @ " 1975...">'
+    expect(s.inspect).to eq('#<UnicodeScanner 0/21 @ "Fri D...">')
+    expect(s.scan_until(/12/)).to eq("Fri Dec 12")
+    expect(s.inspect).to eq('#<UnicodeScanner 10/21 "...ec 12" @ " 1975...">')
   end
 
   it "should pass the #match? example" do
     s = UnicodeScanner.new('test string')
-    s.match?(/\w+/).should == 4
-    s.match?(/\w+/).should == 4
-    s.match?(/\s+/).should == nil
+    expect(s.match?(/\w+/)).to eq(4)
+    expect(s.match?(/\w+/)).to eq(4)
+    expect(s.match?(/\s+/)).to eq(nil)
   end
 
   it "should pass the #matched example" do
     s = UnicodeScanner.new('test string')
-    s.match?(/\w+/).should == 4
-    s.matched.should == "test"
+    expect(s.match?(/\w+/)).to eq(4)
+    expect(s.matched).to eq("test")
   end
 
   it "should pass the #matched? example" do
     s = UnicodeScanner.new('test string')
-    s.match?(/\w+/).should == 4
-    s.matched?.should == true
-    s.match?(/\d+/).should == nil
-    s.matched?.should == false
+    expect(s.match?(/\w+/)).to eq(4)
+    expect(s.matched?).to eq(true)
+    expect(s.match?(/\d+/)).to eq(nil)
+    expect(s.matched?).to eq(false)
   end
 
   it "should pass the #matched_size example" do
     s = UnicodeScanner.new('test string')
-    s.check(/\w+/).should == "test"
-    s.matched_size.should == 4
-    s.check(/\d+/).should == nil
-    s.matched_size.should == nil
+    expect(s.check(/\w+/)).to eq("test")
+    expect(s.matched_size).to eq(4)
+    expect(s.check(/\d+/)).to eq(nil)
+    expect(s.matched_size).to eq(nil)
   end
 
   it "should pass the #peek example" do
     s = UnicodeScanner.new('test string')
-    s.peek(7).should == "test st"
-    s.peek(7).should == "test st"
+    expect(s.peek(7)).to eq("test st")
+    expect(s.peek(7)).to eq("test st")
   end
 
   it "should pass the #pos example" do
     s = UnicodeScanner.new('test string')
-    s.pos.should == 0
-    s.scan_until(/str/).should == "test str"
-    s.pos.should == 8
-    s.terminate.inspect.should == "#<UnicodeScanner fin>"
-    s.pos.should == 11
+    expect(s.pos).to eq(0)
+    expect(s.scan_until(/str/)).to eq("test str")
+    expect(s.pos).to eq(8)
+    expect(s.terminate.inspect).to eq("#<UnicodeScanner fin>")
+    expect(s.pos).to eq(11)
   end
 
   it "should pass the #pos= example" do
     s = UnicodeScanner.new('test string')
-    (s.pos = 7).should == 7
-    s.rest.should == "ring"
+    expect(s.pos = 7).to eq(7)
+    expect(s.rest).to eq("ring")
   end
 
   it "should pass the #post_match/#pre_match example" do
     s = UnicodeScanner.new('test string')
-    s.scan(/\w+/).should == "test"
-    s.scan(/\s+/).should == " "
-    s.pre_match.should == "test"
-    s.post_match.should == "string"
+    expect(s.scan(/\w+/)).to eq("test")
+    expect(s.scan(/\s+/)).to eq(" ")
+    expect(s.pre_match).to eq("test")
+    expect(s.post_match).to eq("string")
   end
 
   it "should pass the #scan example" do
     s = UnicodeScanner.new('test string')
-    s.scan(/\w+/).should == "test"
-    s.scan(/\w+/).should == nil
-    s.scan(/\s+/).should == " "
-    s.scan(/\w+/).should == "string"
-    s.scan(/./).should == nil
+    expect(s.scan(/\w+/)).to eq("test")
+    expect(s.scan(/\w+/)).to eq(nil)
+    expect(s.scan(/\s+/)).to eq(" ")
+    expect(s.scan(/\w+/)).to eq("string")
+    expect(s.scan(/./)).to eq(nil)
   end
 
   it "should pass the #scan_until example" do
     s = UnicodeScanner.new("Fri Dec 12 1975 14:39")
-    s.scan_until(/1/).should == "Fri Dec 1"
-    s.pre_match.should == "Fri Dec "
-    s.scan_until(/XYZ/).should == nil
+    expect(s.scan_until(/1/)).to eq("Fri Dec 1")
+    expect(s.pre_match).to eq("Fri Dec ")
+    expect(s.scan_until(/XYZ/)).to eq(nil)
   end
 
   it "should pass the #skip example" do
     s = UnicodeScanner.new('test string')
-    s.skip(/\w+/).should == 4
-    s.skip(/\w+/).should == nil
-    s.skip(/\s+/).should == 1
-    s.skip(/\w+/).should == 6
-    s.skip(/./).should == nil
+    expect(s.skip(/\w+/)).to eq(4)
+    expect(s.skip(/\w+/)).to eq(nil)
+    expect(s.skip(/\s+/)).to eq(1)
+    expect(s.skip(/\w+/)).to eq(6)
+    expect(s.skip(/./)).to eq(nil)
   end
 
   it "should pass the half-finished #skip_until example" do
     s = UnicodeScanner.new("Fri Dec 12 1975 14:39")
-    s.skip_until(/12/).should == 10
+    expect(s.skip_until(/12/)).to eq(10)
   end
 
   it "should pass the #unscan example" do
     s = UnicodeScanner.new('test string')
-    s.scan(/\w+/).should == "test"
+    expect(s.scan(/\w+/)).to eq("test")
     s.unscan
-    s.scan(/../).should == "te"
-    s.scan(/\d/).should == nil
-    -> { s.unscan }.should raise_error(ScanError, 'unscan failed: previous match record not exist')
+    expect(s.scan(/../)).to eq("te")
+    expect(s.scan(/\d/)).to eq(nil)
+    expect { s.unscan }.to raise_error(ScanError, 'unscan failed: previous match record not exist')
   end
 end
